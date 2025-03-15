642a3f71c1ea658f188118526b7f74e7d859deb3
lsh1202 <vic071202@gmail.com>
2025-03-15 14:48:10 +0900

    :tada: wagwan

---
+++ FakeTimes/FakeTimes/FakeTimesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeTimesApp.swift
+//  FakeTimes
+//
+//  Created by hyk on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct FakeTimesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FakeTimes/FakeTimesTests/FakeTimesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FakeTimesTests.swift
+//  FakeTimesTests
+//
+//  Created by hyk on 3/15/25.
+//
+
+import Testing
+@testable import FakeTimes
+
+struct FakeTimesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
