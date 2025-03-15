81b6111a4a4c38896805428ef804a66dd48c9298
lsh1202 <vic071202@gmail.com>
2025-03-15 16:05:42 +0900

    Custom Button

--- FakeTimes/FakeTimes/Sources/Application/FakeTimesApp.swift
+++ FakeTimes/FakeTimes/Sources/Application/FakeTimesApp.swift
@@ -4,7 +4,9 @@ import SwiftUI
 struct FakeTimesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ContentView()
+            }
         }
     }
 }
---
+++ FakeTimes/FakeTimes/Sources/Components/CusomButton.swift
@@ -0,0 +1,51 @@
+import SwiftUI
+
+struct CusomButton: View {
+    let title: String
+    var destination: AnyView?
+    var action = {}
+    
+    var body: some View {
+        if let destination = destination {
+            NavigationLink(destination: destination) {
+                RoundedRectangle(cornerRadius: 100)
+                    .frame(
+                        minWidth: 335,
+                        maxWidth: 335,
+                        minHeight: 47,
+                        maxHeight: 47
+                    )
+                    .foregroundStyle(Color.primaryNormal)
+                    .overlay {
+                        Text(title)
+                            .font(.headline2Bold)
+                            .foregroundStyle(Color.labelDisable)
+                    }
+            }
+        } else {
+            Button(action: action) {
+                RoundedRectangle(cornerRadius: 100)
+                    .frame(
+                        minWidth: 335,
+                        maxWidth: 335,
+                        minHeight: 47,
+                        maxHeight: 47
+                    )
+                    .foregroundStyle(Color.primaryNormal)
+                    .overlay {
+                        Text(title)
+                            .font(.headline2Bold)
+                            .foregroundStyle(Color.labelDisable)
+                    }
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        CusomButton(title: "Text") {
+            print("yes")
+        }
+    }
+}
